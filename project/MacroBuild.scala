46278745244770595abd978f64bd1b04411fe2ae
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2015-07-17 22:36:09 +0200

    Fixed build (see https://github.com/scalamacros/sbt-example-paradise).

--- project/MacroBuild.scala
+++ project/MacroBuild.scala
@@ -1,11 +1,45 @@
 import sbt._
 import Keys._
 
-object MacroBuild extends Build {
-  lazy val main = Project("main", file(".")) dependsOn macroSub
-  lazy val macroSub = Project("macro", file("macro")) settings(
-    libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
-    libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-reflect" % _),
-    addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0-M5" cross CrossVersion.full)
+object BuildSettings {
+  val paradiseVersion = "2.1.0-M5"
+  val buildSettings = Defaults.defaultSettings ++ Seq(
+    organization := "org.foo",
+    version := "1.0.0",
+    scalacOptions ++= Seq(),
+    scalaVersion := "2.11.6",
+    resolvers += Resolver.sonatypeRepo("snapshots"),
+    resolvers += Resolver.sonatypeRepo("releases"),
+    addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full)
   )
+}
+
+object MyBuild extends Build {
+  import BuildSettings._
+
+  lazy val root: Project = Project(
+    "root",
+    file("."),
+    settings = buildSettings ++ Seq(
+      run <<= run in Compile in core
+    )
+  ) aggregate(macros, core)
+
+  lazy val macros: Project = Project(
+    "macros",
+    file("macros"),
+    settings = buildSettings ++ Seq(
+      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
+      libraryDependencies ++= (
+        if (scalaVersion.value.startsWith("2.10")) List("org.scalamacros" %% "quasiquotes" % paradiseVersion)
+        else Nil
+        )
+    )
+  )
+
+  lazy val core: Project = Project(
+    "core",
+    file("core"),
+    settings = buildSettings
+  ) dependsOn(macros)
 }
